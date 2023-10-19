06c4443a19adb338437bfafe66f41136f27c9a7a
fcrespo8 <fcrespo8@hotmail.com>
2023-10-19 19:27:42 +0200

    removing react import cause not using it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
